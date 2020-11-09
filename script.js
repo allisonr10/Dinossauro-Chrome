3fd75adb66576a5a5583c09211d87f190ae86a4a
Allison Rodrigues <allison.ifg@gmail.com>
2020-11-09 15:23:23 -0300

    Movimentos
    Criando elemento de contato e fim de jogo.

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const dino = document.querySelector('.dino')
 const background = document.querySelector('.background')
 
 let pulando = false
+let posicao = 0
 
 //Adicionar ação de pulo
 
-function pressionar(evento) {
+function pressionar(event) {
   if (event.keyCode === 32) {
     if (!pulando) {
       pulo()
@@ -14,7 +15,6 @@ function pressionar(evento) {
 }
 
 function pulo() {
-  let posicao = 0
   pulando = true
 
   let intervaloSubir = setInterval(() => {
@@ -23,15 +23,14 @@ function pulo() {
 
       //Descer
       let intervaloDescer = setInterval(() => {
-        if (posicao <= 20) {
+        if (posicao <= 0) {
           clearInterval(intervaloDescer)
+          pulando = false // enquando ele estiver no ar, não pode pular novamente.
+        } else {
+          posicao -= 20
+          dino.style.bottom = posicao + 'px'
         }
-        posicao -= 20
-        dino.style.bottom = posicao + 'px'
       }, 20)
-
-      pulando = false // enquando ele estiver no ar, não pode pular novamente.
-
     } else {
       //Subir
       posicao += 20
@@ -45,10 +44,31 @@ function pulo() {
 function criarCactos() {
   const cacto = document.createElement('div')
   let posicaoCacto = 1000
+  let tempoAleatorio = Math.random() * 5500
 
   cacto.classList.add('cacto')
-  cacto.style.left = 1000 + 'px' // cacto vai começar na direita
+  cacto.style.left = posicaoCacto + 'px' // cacto vai começar na direita
   background.appendChild(cacto)
+
+  //movimentar cacto
+
+  let intervaloEsq = setInterval(() => {
+
+    if (posicaoCacto < -60) { // saiu da tela
+      clearInterval(intervaloEsq)
+      background.removeChild(cacto)
+
+    } else if (posicaoCacto > 0 && posicaoCacto < 60 && posicao < 60) {
+      //contato com cacto
+      clearInterval(intervaloEsq)
+      document.body.innerHTML = '<h1 class="game-over"> Fim do Jogo </h1>'
+    } else {
+      posicaoCacto -= 10
+      cacto.style.left = posicaoCacto + 'px'
+    }
+  }, 20)
+
+  setTimeout(criarCactos, tempoAleatorio)
 }
 
 criarCactos()
